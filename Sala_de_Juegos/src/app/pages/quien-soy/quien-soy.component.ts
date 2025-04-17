7313d3bf5fc0efb23b80d7d766e2f233ef8f0f78
Tomas <tomascurto1@gmail.com>
2025-04-17 19:26:02 -0300

    quiensoy y favicon

--- Sala_de_Juegos/src/app/pages/quien-soy/quien-soy.component.ts
+++ Sala_de_Juegos/src/app/pages/quien-soy/quien-soy.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GithubService } from '../../services/github.service';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-quien-soy',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CardModule, ButtonModule],
   templateUrl: './quien-soy.component.html',
-  styleUrl: './quien-soy.component.css'
+  styleUrls: ['./quien-soy.component.css']
 })
-export class QuienSoyComponent {
+export class QuienSoyComponent implements OnInit {
+
+  githubUser: any;
+  username = 'tomascurto';
+
+  constructor(private githubService: GithubService) {}
+
+  ngOnInit(): void {
+    this.githubService.getUserData(this.username).subscribe(data => {
+      this.githubUser = data;
+    });
+  }
 
 }
