bb4c0a0aa10e46dd80d0ce5a24cc409a3e3ad561
Tomas <tomascurto1@gmail.com>
2025-04-16 21:27:57 -0300

    agregadosvarios

--- Sala_de_Juegos/src/app/app.routes.ts
+++ Sala_de_Juegos/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuienSoyComponent } from './pages/quien-soy/quien-soy.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'registro', component: RegistroComponent },
+    { path: 'quien-soy', component: QuienSoyComponent },
+    { path: '**', redirectTo: '' }
+];
---
+++ Sala_de_Juegos/src/app/pages/quien-soy/quien-soy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quien-soy',
+  imports: [],
+  templateUrl: './quien-soy.component.html',
+  styleUrl: './quien-soy.component.css'
+})
+export class QuienSoyComponent {
+
+}
