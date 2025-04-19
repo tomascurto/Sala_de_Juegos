95b39a264f675c3b8ae360f0e2e1e2be8705cc95
Tomas <tomascurto1@gmail.com>
2025-04-19 16:24:39 -0300

    mejora visual de quien soy

--- Sala_de_Juegos/src/main.ts
+++ Sala_de_Juegos/src/main.ts
@@ -5,6 +5,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
